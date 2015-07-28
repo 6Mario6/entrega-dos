bb7b7983117fcf99c4d436e07c96ffaa44fb5cb5
6Mario6 <mario66fernandez@gmail.com>
2015-07-28 16:10:32 -0500

    Nuevo

--- Entrega Dos/src/com/udea/logica/imp/SolicitudPrestamoLogImp.java
+++ Entrega Dos/src/com/udea/logica/imp/SolicitudPrestamoLogImp.java
@@ -2,8 +2,11 @@ package com.udea.logica.imp;
 
 import java.util.List;
 
+import com.udea.dao.UsuarioDAO;
+import com.udea.dto.SolicitudPrestamo;
 import com.udea.dto.Usuario;
 import com.udea.exception.MyException;
+import com.udea.logica.SolicitudPrestamoLogica;
 
 /**<h1> SolicitudPrestamoLogica</h1>
  * Es una interfaz la cual sirve para definir las funciones de la logica del negocio de las solicitudes
@@ -14,6 +17,28 @@ import com.udea.exception.MyException;
  * @since 01-06-15
  *
  */
-public class SolicitudPrestamoLogImp {
+public class SolicitudPrestamoLogImp implements SolicitudPrestamoLogica {
+	private UsuarioDAO usDao;
+
+	@Override
+	public List<SolicitudPrestamo> obtener() throws MyException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SolicitudPrestamoLogImp obtenerSolicitudPrestamoLogica(
+			Integer idReservacion) throws MyException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void realizarSolicitud(int idReservacion, String nombre,
+			String apellido, String telefono, String email, String username,
+			String password, String grupoInvestigacion) throws MyException {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
